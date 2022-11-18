ea626f017dcdf8510cc2768aa9ce486c1538dd48
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2022-11-18 04:28:30 -0800

    Add files via upload

--- golden_ratio_approximation.cpp
+++ golden_ratio_approximation.cpp
@@ -7,14 +7,9 @@
  */
 
 /* preprocessing directives */
-#include <iostream> // command line input and output
-#include <fstream> // file input and output
-#define MAXIMUM_N 93 // constant which represents maximum N value
-
-/* preprocessing directives */
 #include <iostream> // library for defining objects which handle command line input and command line output
 #include <fstream> // library for defining objects which handle file input and file output
-#define MAXIMUM_N 93 // constant which represents maximum N value
+#define MAXIMUM_N 45 // constant which represents maximum N value
 
 /* function prototypes */
 unsigned long long int compute_Nth_fibonacci_sequence_term_using_iteration(int N);
