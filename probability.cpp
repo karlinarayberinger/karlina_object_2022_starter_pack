a36aaa5b1033829f026576af61dc216b86be9989
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2022-11-18 19:51:11 -0800

    Add files via upload

--- probability.cpp
+++ probability.cpp
@@ -57,3 +57,213 @@ void bubble_sort(int * A, int S)
         if (!adjacent_elements_were_swapped) array_is_sorted = true;
     }
 }
+
+/* program entry point */
+int main()
+{
+    // Declare three int type variables and set each of their initial values to 0.
+    int S = 0, T = 0, i = 0;
+
+    // Declare a pointer-to-int type variable.
+    int * A;
+
+    // Declare a file output stream object.
+    std::ofstream file;
+
+    /**
+     * If probability_output.txt does not already exist in the same directory as probability.cpp, 
+     * create a new file named probability_output.txt.
+     * 
+     * Open the plain-text file named probability_output.txt 
+     * and set that file to be overwritten with program data.
+     */
+    file.open("probability_output.txt");
+
+    // Print an opening message to the command line terminal.
+    std::cout << "\n\n--------------------------------";
+    std::cout << "\nStart Of Program";
+    std::cout << "\n--------------------------------";
+
+    // Print an opening message to the file output stream.
+    file << "--------------------------------";
+    file << "\nStart Of Program";
+    file << "\n--------------------------------";
+
+    // Print "The following statements describe the data capacities of various primitive C++ data types:" to the command line terminal.
+    std::cout << "\n\nThe following statements describe the data capacities of various primitive C++ data types:";
+
+    // Print "The following statements describe the data capacities of various primitive C++ data types:" to the file output stream.
+    file << "\n\nThe following statements describe the data capacities of various primitive C++ data types:";
+
+    // Print the data size of an int type variable to the command line terminal.
+    std::cout << "\n\nsizeof(int) = " << sizeof(int) << " byte(s). // integer (i.e. whole number) values";
+
+    // Print the data size of an int type variable to the file output stream.
+    file << "\n\nsizeof(int) = " << sizeof(int) << " byte(s). // integer (i.e. whole number) values";
+
+    // Print the data size of an unsigned long long int type variable to the command line terminal.
+    std::cout << "\n\nsizeof(unsigned long long int) = " << sizeof(unsigned long long int) << " byte(s). // integer values from a larger set than what int encompasses";
+
+    // Print the data size of an unsigned long long int type variable to the file output stream.
+    file << "\n\nsizeof(unsigned long long int) = " << sizeof(unsigned long long int) << " byte(s). // integer values from a larger set that what int encompasses";
+
+    // Print the data size of a bool type variable to the command line terminal.
+    std::cout << "\n\nsizeof(bool) = " << sizeof(bool) << " byte(s). // Boolean values (true : not(0) :: false : 0)";
+
+    // Print the data size of a bool type variable to the file output stream.
+    file << "\n\nsizeof(bool) = " << sizeof(bool) << " byte(s).";
+
+    // Print the data size of a char type variable to the command line terminal.
+    std::cout << "\n\nsizeof(char) = " << sizeof(char) << " byte(s).";
+
+    // Print the data size of a char type variable to the file output stream.
+    file << "\n\nsizeof(char) = " << sizeof(char) << " byte(s).";
+
+    // Print the data size of a float type variable to the command line terminal.
+    std::cout << "\n\nsizeof(float) = " << sizeof(float) << " byte(s).";
+
+    // Print the data size of a float type variable to the file output stream.
+    file << "\n\nsizeof(float) = " << sizeof(float) << " byte(s).";
+
+    // Print the data size of a double type variable to the command line terminal.
+    std::cout << "\n\nsizeof(double) = " << sizeof(double) << " byte(s).";
+
+    // Print the data size of a double type variable to the file output stream.
+    file << "\n\nsizeof(double) = " << sizeof(double) << " byte(s).";
+
+    // Print the data size of a long double type variable to the command line terminal.
+    std::cout << "\n\nsizeof(long double) = " << sizeof(long double) << " byte(s).";
+
+    // Print the data size of a long double type variable to the file output stream.
+    file << "\n\nsizeof(long double) = " << sizeof(long double) << " byte(s).";
+
+    // Print the data size of a pointer-to-int type variable to the command line terminal.
+    std::cout << "\n\nsizeof(int *) = " << sizeof(int *) << " byte(s).";
+
+    // Print the data size of a pointer-to-int type variable to the file output stream.
+    file << "\n\nsizeof(int *) = " << sizeof(int *) << " byte(s).";
+
+    // Print the data size of a pointer-to-unsigned-long-long-int type variable to the command line terminal.
+    std::cout << "\n\nsizeof(unsigned long long int *) = " << sizeof(unsigned long long int *) << " byte(s).";
+
+    // Print the data size of a pointer-to-unsigned-long-long-int type variable to the file output stream.
+    file << "\n\nsizeof(unsigned long long int *) = " << sizeof(unsigned long long int *) << " byte(s).";
+
+    // Print the data size of a pointer-to-bool type variable to the command line terminal.
+    std::cout << "\n\nsizeof(bool *) = " << sizeof(bool *) << " byte(s).";
+
+    // Print the data size of a pointer-to-bool type variable to the file output stream.
+    file << "\n\nsizeof(bool *) = " << sizeof(bool *) << " byte(s).";
+
+    // Print the data size of a pointer-to-char type variable to the command line terminal.
+    std::cout << "\n\nsizeof(char *) = " << sizeof(char *) << " byte(s).";
+
+    // Print the data size of a pointer-to-char type variable to the file output stream.
+    file << "\n\nsizeof(char *) = " << sizeof(char *) << " byte(s).";
+
+    // Print the data size of a pointer-to-float type variable to the command line terminal.
+    std::cout << "\n\nsizeof(float *) = " << sizeof(float *) << " byte(s).";
+
+    // Print the data size of a pointer-to-float type variable to the file output stream.
+    file << "\n\nsizeof(float *) = " << sizeof(float *) << " byte(s).";
+
+    // Print the data size of a pointer-to-double type variable to the command line terminal.
+    std::cout << "\n\nsizeof(double *) = " << sizeof(double *) << " byte(s).";
+
+    // Print the data size of a pointer-to-double type variable to the file output stream.
+    file << "\n\nsizeof(double *) = " << sizeof(double *) << " byte(s).";
+
+    // Print the data size of a pointer-to-long-double type variable to the command line terminal.
+    std::cout << "\n\nsizeof(long double *) = " << sizeof(long double *) << " byte(s).";
+
+    // Print the data size of a pointer-to-long-double type variable to the file output stream.
+    file << "\n\nsizeof(long double *) = " << sizeof(long double *) << " byte(s).";
+
+    // Print the data size of a pointer-to-pointer-to-int type variable to the command line terminal.
+    std::cout << "\n\nsizeof(int **) = " << sizeof(int **) << " byte(s).";
+
+    // Print the data size of a pointer-to-pointer-to-int type variable to the file output stream.
+    file << "\n\nsizeof(int **) = " << sizeof(int **) << " byte(s).";
+
+    // Print a horizontal line to the command line terminal.
+    std::cout << "\n\n--------------------------------";
+
+    // Print a horizontal line to the command line terminal.
+    file << "\n\n--------------------------------";
+
+    // Print "Enter a natural number, S, for representing the number of elements to include in an array which is no larger than than {MAXIMUM_S}: " to the command line terminal.
+    std::cout << "\n\nEnter a natural number, S, for representing the number of elements to include in an array which is no larger than " << MAXIMUM_S << ": ";
+
+    // Scan the command line terminal for the most recent keyboard input value.
+    std::cin >> S;
+
+    // Print "The value which was entered for S is {S}." to the command line terminal.
+    std::cout << "\nThe value which was entered for S is " << S << ".";
+
+    // Print "The value which was entered for S is {S}." to the file output stream.
+    file << "\n\nThe value which was entered for S is " << S << ".";
+
+    // If S is smaller than 1 or if S is larger than MAXIMUM_S, set S to 10.
+    S = ((S < 1) || (S > MAXIMUM_S)) ? 10 : S; 
+
+    // Print "S := {S}. // number of consecutive int-sized chunks of memory to allocate to an array such that the memory address of the first element of that array, A[0], is stored in a pointer-to-int type variable named A" to the command line terminal.
+    std::cout << "\n\nS := " << S << ". // number of consecutive int-sized chunks of memory to allocate to an array such that the memory address of the first element of that array, A[0], is stored in a pointer-to-int type variable named A";
+
+    // Print "S := {S}. // number of consecutive int-sized chunks of memory to allocate to an array such that the memory address of the first element of that array, A[0], is stored in a pointer-to-int type variable named A" to the file output stream.
+    file << "\n\nS := " << S << ". // number of consecutive int-sized chunks of memory to allocate to an array such that the memory address of the first element of that array, A[0], is stored in a pointer-to-int type variable named A";
+
+    // ...
+    bool X = 77;
+    std:cout << "\n\nTest: bool X = 77. What is X? " << X;
+
+    // Print a horizontal line to the command line terminal.
+    std::cout << "\n\n--------------------------------";
+
+    // Print a horizontal line to the command line terminal.
+    file << "\n\n--------------------------------";
+
+    // Print "Enter a natural number, T, for representing the number of unique states which each element of the array can store exactly one of which is no larger than {MAXIMUM_T}: " to the command line terminal.
+    std::cout << "\n\nEnter a natural number, T, for representing the number of unique states which each element of the array can store exactly one of which is no larger than " << MAXIMUM_T << ": ";
+
+    // Scan the command line terminal for the most recent keyboard input value.
+    std::cin >> T;
+
+    // Print "The value which was entered for T is {T}." to the command line terminal.
+    std::cout << "\nThe value which was entered for T is " << T << ".";
+
+    // Print "The value which was entered for T is {T}." to the file output stream.
+    file << "\n\nThe value which was entered for T is " << T << ".";
+
+    // If T is smaller than 1 or if T is larger than MAXIMUM_T, set T to 100.
+    T = ((T < 1) || (T > MAXIMUM_T)) ? 100 : T; 
+
+    // Print "T := {T}. // number of unique states which each element of the array pointed to by A can store exactly one of at a time" to the command line terminal.
+    std::cout << "\n\nT := " << T << ". // number of unique states which each element of the array pointed to by A can store exactly one of at a time";
+
+    // Print "T := {T}. /// number of unique states which each element of the array pointed to by A can store exactly one of at a time" to the file output stream.
+    file << "\n\nT := " << T << ". // number of unique states which each element of the array pointed to by A can store exactly one of at a time";
+
+    // Print a horizontal line to the command line terminal.
+    std::cout << "\n\n--------------------------------";
+
+    // Print a horizontal line to the command line terminal.
+    file << "\n\n--------------------------------";
+
+    // ...
+
+    // Print a closing message to the command line terminal.
+    std::cout << "\n\n--------------------------------";
+    std::cout << "\nEnd Of Program";
+    std::cout << "\n--------------------------------\n\n";
+
+    // Print a closing message to the file output stream.
+    file << "\n\n--------------------------------";
+    file << "\nEnd Of Program";
+    file << "\n--------------------------------";
+
+    // Close the file output stream.
+    file.close();
+
+    // Exit the program.
+    return 0;
+}
