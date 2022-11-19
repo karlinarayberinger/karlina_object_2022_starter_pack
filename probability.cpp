60efdb84187e4d618397900b3c8df942008ed725
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2022-11-18 18:55:25 -0800

    Add files via upload

--- probability.cpp
+++ probability.cpp
@@ -18,3 +18,42 @@
 /* function prototypes */
 void bubble_sort(int * A, int S);
 void print_histogram(int * A, int S, int T, std::ostream & output);
+
+/**
+ * Use the Bubble Sort algorithm to arrange the elements of 
+ * an int type array, A, in ascending order
+ * such that A[0] represents the smallest integer value in that array and 
+ * such that A[S - 1] represents the largest integer value in that array.
+ * 
+ * Assume that S is a natural number no larger than MAXIMUM_S.
+ * 
+ * Assume that A is a pointer to an int type variable and that 
+ * A stores the memory address of the first element, A[0], 
+ * of an int type array comprised of exactly S elements.
+ * (In other words, assume that exactly S consecutive int-sized 
+ * chunks of memory are allocated to the array represented by A).
+ * 
+ * Although this function returns no value, 
+ * the array which the pointer variable, A, points to is updated 
+ * if the elements of that array are not already arranged in ascending order. 
+ */
+void bubble_sort(int * A, int S)
+{
+    int i = 0, placeholder = 0; 
+    bool array_is_sorted = false, adjacent_elements_were_swapped = false;
+    while (!array_is_sorted)
+    {
+        adjacent_elements_were_swapped = false;
+        for (i = 1; i < S; i += 1)
+        {
+            if (A[i] < A[i - 1])
+            {
+                placeholder = A[i];
+                A[i] = A[i - 1];
+                A[i - 1] = placeholder;
+                adjacent_elements_were_swapped = true;
+            }
+        }
+        if (!adjacent_elements_were_swapped) array_is_sorted = true;
+    }
+}
