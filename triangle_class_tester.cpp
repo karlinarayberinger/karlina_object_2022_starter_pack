e07dfcc57e573a4437a48e4d298811987abe1e10
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2022-11-20 21:26:41 -0800

    Add files via upload

---
+++ triangle_class_tester.cpp
@@ -0,0 +1,76 @@
+/**
+ * file: triangle_class_tester.cpp
+ * type: C++ (source file)
+ * date: 20_NOVEMBER_2022
+ * author: Karlina Ray Beringer
+ * license: PUBLIC_DOMAIN 
+ */
+
+#include "triangle.h" // Include the C++ header file which contains preprocessing directives, variable declarations, and function prototypes for the TRIANGLE class.
+
+/* function prototypes */
+void unit_test_0(std::ostream & output);
+
+// Unit Test # 0: TRIANGLE class default constructor, TRIANGLE class print method, and TRIANGLE class destructor.
+void unit_test_0(std::ostream & output) 
+{
+    output << "\n\n--------------------------------------------------------------------------------------------------";
+    output << "\nUnit Test # 0: TRIANGLE class default constructor, TRIANGLE class print method, and TRIANGLE class destructor.";
+    output << "\n--------------------------------------------------------------------------------------------------";
+    output << "\nTRIANGLE point;";
+    output << "\ntriangle.print(output);";
+    TRIANGLE triangle;
+    TRIANGLE.print(output);
+}
+
+/* program entry point */
+int main()
+{
+    // Declare a file output stream object.
+    std::ofstream file;
+
+    // Set the number of digits of floating-point numbers which are printed to the command line terminal to 100 digits.
+    std::cout.precision(100);
+
+    // Set the number of digits of floating-point numbers which are printed to the file output stream to 100 digits.
+    file.precision(100);
+
+    /**
+     * If triangle_class_tester_output.txt does not already exist in the same directory as triangle_class_tester.cpp, 
+     * create a new file named triangle_class_tester_output.txt.
+     * 
+     * Open the plain-text file named triangle_class_tester_output.txt 
+     * and set that file to be overwritten with program data.
+     */
+    file.open("triangle_class_tester_output.txt");
+
+    // Print an opening message to the command line terminal.
+    std::cout << "\n\n--------------------------------";
+    std::cout << "\nStart Of Program";
+    std::cout << "\n--------------------------------";
+
+    // Print an opening message to the file output stream.
+    file << "--------------------------------";
+    file << "\nStart Of Program";
+    file << "\n--------------------------------";
+
+    // Implement a series of unit tests which demonstrate the functionality of TRIANGLE class variables.
+    unit_test_0(std::cout);
+    unit_test_0(file);
+
+    // Print a closing message to the command line terminal.
+    std::cout << "\n\n--------------------------------";
+    std::cout << "\nEnd Of Program";
+    std::cout << "\n--------------------------------\n\n";
+
+    // Print a closing message to the file output stream.
+    file << "\n\n--------------------------------";
+    file << "\nEnd Of Program";
+    file << "\n--------------------------------";
+
+    // Close the file output stream.
+    file.close();
+
+    // Exit the program.
+    return 0;
+}
