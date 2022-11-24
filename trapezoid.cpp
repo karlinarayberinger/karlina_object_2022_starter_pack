a59fcfed54243fcbfe6fd304691b7620c40bb070
Karlina Ray Beringer <karlina.ray.beringer@protonmail.com>
2022-11-24 02:20:55 -0800

    Add files via upload

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -60,6 +60,7 @@ bool TRAPEZOID::is_trapezoid()
 
     if (!points_represent_unique_coordinate_pairs(A,B,C,D) || !interior_angles_add_up_to_360_degrees()) return false;
     if ((a == c) && (b != d) && (slope_of_a != slope_of_c) && (slope_of_b == slope_of_d)) return true;
+    if ((a != c) && (b == d) && (slope_of_a == slope_of_c) && (slope_of_b != slope_of_d)) return true;
     return false;
 }
 
